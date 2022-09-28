18f388ae532aaa82a75aa8a8089be879105be7dc
Pranshu Sahu <sahupranshu637@gmail.com>
2022-09-29 00:48:11 +0530

    work accessibility issue

--- script2.js
+++ script2.js
@@ -1,14 +1,16 @@
 const theme = document.getElementById("theme");
 
 // change theme
-theme.addEventListener("click", () => {
+theme.addEventListener("click", toggleTheme);
+theme.addEventListener("keydown", toggleTheme);
+function toggleTheme() {
   document.body.classList.toggle("dark");
   if (document.body.classList.contains("dark")) {
     theme.setAttribute("src", "images/icon-sun.svg");
   } else {
     theme.setAttribute("src", "images/icon-moon.svg");
   }
-});
+};
 // function to clear completed tasks
 function clearCompletedTask() {
   todosArray = [...todosArray.filter((task) => !task.completed)];
